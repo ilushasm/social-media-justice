9a0dc25f00b79b59aa4caa62ae26772e18131f07
Illia Smyrnov <ilushasm@gmail.com>
2023-08-24 22:42:29 +0300

    Added Logout and LogoutAll views

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from user.views import CreateUserView, ProfileUserView
+from user.views import CreateUserView, ProfileUserView, LogoutView, LogoutAllView
 
 urlpatterns = [
     path(
@@ -17,6 +17,8 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("logout/", LogoutView.as_view(), name="token_logout"),
+    path("logout_all/", LogoutAllView.as_view(), name="auth_logout_all"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,8 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, views, status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.settings import api_settings
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import RefreshToken, OutstandingToken, BlacklistedToken
 
 from user.models import User
 from user.serializers import UserSerializer, UserAuthTokenSerializer
@@ -15,6 +18,33 @@ class CreateUserView(generics.CreateAPIView):
 class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     serializer_class = UserAuthTokenSerializer
+
+
+class LogoutView(views.APIView):
+    permission_classes = (IsAuthenticated,)
+
+    @staticmethod
+    def post(request) -> Response:
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except TokenError as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutAllView(views.APIView):
+    permission_classes = (IsAuthenticated,)
+
+    @staticmethod
+    def post(request) -> Response:
+        tokens = OutstandingToken.objects.filter(user_id=request.user.id)
+        for token in tokens:
+            t, _ = BlacklistedToken.objects.get_or_create(token=token)
+
+        return Response(status=status.HTTP_205_RESET_CONTENT)
 
 
 class ProfileUserView(generics.RetrieveUpdateAPIView):
